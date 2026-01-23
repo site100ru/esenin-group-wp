6afad4e2788df0908c6c03463f5dbaa1b2707d15
Roman Vasilyev <vasilyev-r@mail.ru>
2026-01-23 21:35:55 +0300

    Test autodeploy

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
 			for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
 			k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
 			ym(95629828, "init", {
 				clickmap:true,
 				trackLinks:true,
